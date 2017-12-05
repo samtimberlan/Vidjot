8bba2e6ec7f0617542746c494a7c47544b3b2e50
DESKTOP-G6T2AL6\timud <timudoma@gmail.com>
2017-12-05 11:25:12 +0100

    Add basic routes

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const express = require('express');
 
 const app = express();
 
+//Index Route
+app.get('/', (req, res)=>{
+  res.send('index');
+});
+
+app.get('/about', (req, res)=>{
+  res.send('About');
+});
+
 const port = 5000;
 
 app.listen(port, () => {
