88b94e0d50f0a32ed685f885e5374e305de9bd7f
DESKTOP-G6T2AL6\timud <timudoma@gmail.com>
2017-12-05 15:31:50 +0100

    Add Mongoose Connection

--- app.js
+++ app.js
@@ -1,27 +1,38 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
+const express = require("express");
+const exphbs = require("express-handlebars");
+const mongoose = require("mongoose");
 
 const app = express();
 
-//Handlebars middleware
-app.engine('handlebars', exphbs({
-  defaultLayout: 'main'
-}));
-app.set('view engine', 'handlebars');
+//Map global promise - get rid of warning
+mongoose.Promise = global.Promise;
+//Connect to Mongoose
+mongoose.connect("mongodb://localhost/vidjot-dev", {
+  useMongoClient: true
+}).then(()=>{console.log('MongoDB Connected...')})
+.catch(err => console.log(err));
 
+//Handlebars middleware
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
+app.set("view engine", "handlebars");
 
 //Index Route
-app.get('/', (req, res)=>{
-  const title = 'Welcome';
-  res.render('index', {title: title});
+app.get("/", (req, res) => {
+  const title = "Welcome";
+  res.render("index", { title: title });
 });
 
-app.get('/about', (req, res)=>{
-  res.render('about');
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
 const port = 5000;
 
 app.listen(port, () => {
-  console.log(`Server started on port ${port}`)
+  console.log(`Server started on port ${port}`);
 });
