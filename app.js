2e1ec3c32d1888ce7b9feede0d10aed991cf7618
DESKTOP-G6T2AL6\timud <timudoma@gmail.com>
2017-12-05 13:40:53 +0100

    Add express-handlebars

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 
 const app = express();
 
+//Handlebars middleware
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+
 //Index Route
 app.get('/', (req, res)=>{
-  res.send('index');
+  const title = 'Welcome1';
+  res.render('index', {title: title});
 });
 
 app.get('/about', (req, res)=>{
-  res.send('About');
+  res.render('about');
 });
 
 const port = 5000;
