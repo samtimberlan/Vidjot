53d9bc445a3c4ec197ad924372e300b02cd9b797
DESKTOP-G6T2AL6\timud <timudoma@gmail.com>
2017-12-19 13:54:00 +0100

    Add Password encryption

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+//const passport = require('passport');
+
+//Load User model
+require("../models/User");
+const User = mongoose.model("users");
 
 //User Login Route
 router.get("/login", (req, res) => {
@@ -18,20 +24,51 @@ router.post("/register", (req, res) => {
   if (req.body.password !== req.body.password2) {
     errors.push({ text: "Passwords do not match" });
   }
-  if(req.body.password.length < 5 ){
-    errors.push({text : 'Password must be at least 5 characters'});
+  if (req.body.password.length < 5) {
+    errors.push({ text: "Password must be at least 5 characters" });
   }
 
-  if(errors.length > 0){
-    res.render('users/register', {
-      errors : errors,
+  if (errors.length > 0) {
+    res.render("users/register", {
+      errors: errors,
       name: req.body.name,
       email: req.body.email,
       password: req.body.password,
       password2: req.body.password2
     });
-  } else{
-    res.send('passed');
+  } else {
+    User.findOne({ email: req.body.email }).then(user => {
+      if (user) {
+        req.flash("errorMsg", "Email already registered");
+        res.redirect("register");
+      } else {
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password
+        });
+
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser
+              .save()
+              .then(user => {
+                req.flash(
+                  "successMsg",
+                  "You are now registered and can now log in"
+                );
+                res.redirect("login");
+              })
+              .catch(err => {
+                console.log(err);
+                return;
+              });
+          });
+        });
+      }
+    });
   }
 });
 
