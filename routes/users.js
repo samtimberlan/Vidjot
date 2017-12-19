f8df8b100773d5e31895f39aad12ef55bbb237aa
DESKTOP-G6T2AL6\timud <timudoma@gmail.com>
2017-12-19 12:32:49 +0100

    Add validation to register form

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,38 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 //User Login Route
-router.get('/login', (req, res)=>{
-  res.render('users/login');
+router.get("/login", (req, res) => {
+  res.render("users/login");
 });
 
 //User Register Route
-router.get('/register', (req, res)=>{
-  res.render('users/register');
+router.get("/register", (req, res) => {
+  res.render("users/register");
+});
+
+//Register Form POST
+router.post("/register", (req, res) => {
+  let errors = [];
+  if (req.body.password !== req.body.password2) {
+    errors.push({ text: "Passwords do not match" });
+  }
+  if(req.body.password.length < 5 ){
+    errors.push({text : 'Password must be at least 5 characters'});
+  }
+
+  if(errors.length > 0){
+    res.render('users/register', {
+      errors : errors,
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      password2: req.body.password2
+    });
+  } else{
+    res.send('passed');
+  }
 });
 
 module.exports = router;
